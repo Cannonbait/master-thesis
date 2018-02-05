059624c1d86cbc1ddd0461780bdb982a98533297
Cannonbait <ivar_busig@hotmail.com>
2018-02-05 16:50:18 +0100

    Added threading example

--- master-thesis/main.cpp
+++ master-thesis/main.cpp
@@ -9,7 +9,7 @@ const int NUM_PATTERNS = 25;
 const int NUM_STORED_ITEMS = 100;
 const int NUM_BLOCKS = 25;
 
-int main() {
+int main23() {
   PatternBF a = *(new PatternBF(NUM_PATTERNS, NUM_STORED_ITEMS, NUM_BLOCKS));
   a.add_many(NUM_STORED_ITEMS);
   cout << a.test_rng(10000000) << endl;
