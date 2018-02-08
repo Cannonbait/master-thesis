550549e75b713015dde3e5df1187fc4da734712e
Cannonbait <ivar_busig@hotmail.com>
2018-02-08 16:17:23 +0100

    Renamed setup files depending on version

--- code-framework/cython/setup_framework_win.py
+++ code-framework/cython/setup_framework_win.py
@@ -2,9 +2,9 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
 setup(ext_modules = cythonize(Extension(
-           "pbloom",                                # the extension name
-           sources=["pbloom.pyx", "../core/patternbloom.cpp"], # the Cython source and
+           "framework",                                # the extension name
+           sources=["framework.pyx", "../core/filter_framework.cpp","../core/worker.cpp","../core/patternbloom.cpp"], # the Cython source and
                                                   # additional C++ source files
            language="c++",                        # generate and compile C++ code
-           extra_compile_args=["-std=c++14","/IC:/MinGW/include"],
+           extra_compile_args=["/IC:/MinGW/include"],
       )))
