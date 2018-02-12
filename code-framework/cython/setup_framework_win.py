f14dad62060ba4ec117e3a0f63f70afb2bb0661c
Cannonbait <ivar_busig@hotmail.com>
2018-02-12 11:31:18 +0100

    Include numpy dir and remove worker.cpp dependency

--- code-framework/cython/setup_framework_win.py
+++ code-framework/cython/setup_framework_win.py
@@ -1,10 +1,12 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
+import numpy
 
 setup(ext_modules = cythonize(Extension(
            "framework",                                # the extension name
-           sources=["framework.pyx", "../core/filter_framework.cpp","../core/worker.cpp","../core/patternbloom.cpp"], # the Cython source and
+           sources=["framework.pyx", "../core/filter_framework.cpp","../core/patternbloom.cpp"], # the Cython source and
                                                   # additional C++ source files
            language="c++",                        # generate and compile C++ code
            extra_compile_args=["/IC:/MinGW/include"],
+           include_dirs=[numpy.get_include()]
       )))
