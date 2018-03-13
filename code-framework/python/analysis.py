6226394381873c9be447aa3253ddc437c012463b
Björn Hedström <bjorn@hfinfo.se>
2018-03-13 11:14:08 +0100

    Optimization of the analysis tool

--- code-framework/python/analysis.py
+++ code-framework/python/analysis.py
@@ -7,13 +7,13 @@ import numpy as np
 import framework
 import pattern_designs
 from mpl_toolkits.mplot3d import Axes3D
-sys.argv[1:] = ["-source=aaa", "-m=512", "-n=10", "-d=100", "-d_end=103", "-b=30"]
+sys.argv[1:] = ["-m=509", "-n=6400", "-d=100", "-d_end=150", "-b=1"]
 
 
 ######################## PARSE ARGUMENTS
 def default_arguments():
     arguments = {"m": 512, "n": 800, "d": 200, "b":30}
-    arguments["tests"] = 1000
+    arguments["tests"] = 90000
     arguments["pattern_trials"] = 5
     return arguments
 
@@ -44,9 +44,7 @@ class Analysis_Settings:
             print("Found no \"source\" argument, trials will be run with random input")
             self.source = None
 
-
-
-######################## GENERATE DATA 
+######################## GENERATE DATA
 def generate_dimensions(settings):
     dimensions = []
     for key in settings.trial_ranges:
@@ -56,17 +54,18 @@ def generate_dimensions(settings):
 
 def run_trial(trial_parameters, num_pattern_trials, num_framework_tests):
     average = 0.0
+    bits = trial_parameters["m"]
+    patterns = trial_parameters["n"]
+    stored = trial_parameters["d"]
+    blocks = trial_parameters["b"]
+    f = framework.PyFilterFramework(bits, patterns, stored, blocks)
     for trial in range(0, num_pattern_trials):
-        bits = trial_parameters["m"]
-        patterns = trial_parameters["n"]
-        stored = trial_parameters["d"]
-        blocks = trial_parameters["b"]
-        f = framework.PyFilterFramework(bits, patterns, stored, blocks)
+        print("d: ", stored, "trial: ", trial)
         f.replace_patterns(pattern_designs.comp(bits, patterns, stored, blocks), blocks)
         f.add_items(stored)
         average = average + f.test_framework(num_framework_tests)
     return average / num_pattern_trials
-                
+
 def generate_data(settings):
     dimensions = generate_dimensions(settings)
     parameters = {}
@@ -74,41 +73,40 @@ def generate_data(settings):
     for key in settings.trial_ranges:
         if len(settings.trial_ranges[key]) == 1:
             parameters[key] = settings.trial_ranges[key][0]
-
     # If we have no ranges
     if len(dimensions) == 0:
         print("No ranges not implemented")
         sys.exit(0)
     elif len(dimensions) == 1:
         fpr = np.zeros(dimensions[0][1].size)
+        trial_parameters = parameters.copy()
         for ix, x in enumerate(dimensions[0][1]):
-            trial_parameters = parameters.copy()
             trial_parameters[dimensions[0][0]] = x
             fpr[ix] = run_trial(trial_parameters, settings.pattern_trials, settings.tests)
         return fpr
-    
+
     else:
         fpr = np.zeros([dimensions[0][1].size, dimensions[1][1].size])
+        trial_parameters = parameters.copy()
         for ix, x in enumerate(dimensions[0][1]):
             for iy, y in enumerate(dimensions[1][1]):
-                trial_parameters = parameters.copy()
                 trial_parameters[dimensions[0][0]] = x
                 trial_parameters[dimensions[1][0]] = y
                 fpr[ix][iy] = run_trial(trial_parameters, settings.pattern_trials, settings.tests)
-            
         return fpr
-
-
 
 ######################## DISPLAY DATA
 
-def display_data(result, dimensions):
+def display_data(result, setting):
+    dimensions = generate_dimensions(settings)
     if len(dimensions) == 1:
         fig, ax = plt.subplots()
         x = dimensions[0][1]
         ax.plot(x,result)
         plt.xlabel(dimensions[0][0])
-        plt.show()    
+        plt.ylabel("FPR")
+        plt.title("False positive rate as a funtion of ", dimensions[0][0])
+        plt.show()
     else:
         fig = plt.figure()
         ax = fig.gca(projection='3d')
@@ -116,12 +114,9 @@ def display_data(result, dimensions):
         ax.plot_surface(x,y,result)
         plt.xlabel(dimensions[1][0])
         plt.ylabel(dimensions[0][0])
-        plt.show()    
+        plt.show()
 
-        
+
 settings = Analysis_Settings(sys.argv)
-
 result = generate_data(settings)
-display_data(result, generate_dimensions(settings))
-
-
+display_data(result, settings)
