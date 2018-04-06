88167d1e93e63c2e714ea0af488578b2d4b4ffd8
Björn Hedström <bjorn@hfinfo.se>
2018-04-06 09:46:21 +0200

    Fixed the testing from source. WOrks in C++ but cython binding is broken

--- code-framework/core/serial_framework.cpp
+++ code-framework/core/serial_framework.cpp
@@ -4,15 +4,22 @@
 #include <thread>
 #include <chrono>
 #include <boost/algorithm/string.hpp>
+#include <gmp.h>
+#include <algorithm>
+#include <boost/random.hpp>
+#include <random>
+
 using namespace std;
 
 
 SerialFramework::SerialFramework() {}
-SerialFramework::SerialFramework(string path){
+
+SerialFramework::SerialFramework(string path) {
+  random_source = boost::mt19937(std::chrono::system_clock::now().time_since_epoch().count()*47);
   ifstream data_file(path);
   string line;
-  while (getline(data_file, line)){
-    source.push_back(stoi(line.substr(0, 8)));
+  while (getline(data_file, line)) {
+    source.push_back(line);
   }
 }
 
@@ -20,31 +27,55 @@ double SerialFramework::test(vector<vector<bool>> patterns,
           int bits, int store, int blocks, int tests){
   //Create pattern
   PatternBF bf(convert_patterns(patterns), blocks);
-
-  if (source.size() == 0){
+  if (source.size() == 0) {
     bf.add_many(store);
     return bf.test_rng(tests);
-  }
-  else{
+  } else {
     //Store data
-    for(int i = 0; i < store; i++){
+    mpz_t block_size, patterns_size, value, block_i, pattern_i;
+    mpz_init(value);
+    mpz_init(block_i);
+    mpz_init(pattern_i);
+
+    int i1, i2, universe, storage_index;
+    universe = source.size()-1;
+    std::uniform_int_distribution<int> dist_blk(0, universe);
+    for(int i = 0; i < store; i++) {
+      storage_index = dist_blk(random_source) % universe;
+      stored.push_back(source[storage_index]);
+      mpz_set_str(value, source[storage_index].c_str(), 10);
       // Should possibly be changed to random lines
-      int i1 = source[i] % blocks;
-      int i2 = source[i] % patterns.size();
-      bf.add_indexes((source[i] % blocks), (source[i] % patterns.size()));
+      mpz_mod_ui(block_i, value, blocks);
+      mpz_mod_ui(pattern_i, value, patterns.size());
+      i1 = mpz_get_ui(block_i);
+      i2 = mpz_get_ui(pattern_i);
+      bf.add_indexes(i1, i2);
     }
 
     int contained = 0;
-    for(int i = store; i < (store+tests); i++){
-      if(bf.test(source[i] % blocks, source[i] % patterns.size())){
-        contained++;
+    for(int i = store; i < (store+tests); i++) {
+      storage_index = dist_blk(random_source) % universe;
+      mpz_set_str(value, source[storage_index].c_str(), 10);
+      mpz_mod_ui(block_i, value, blocks);
+      mpz_mod_ui(pattern_i, value, patterns.size());
+      i1 = mpz_get_ui(block_i);
+      i2 = mpz_get_ui(pattern_i);
+
+      // The element is a true positive and should not be logged for FPR
+      if (!(find(stored.begin(), stored.end(), source[i]) != stored.end())) {
+        if (bf.test(i1,i2)) {
+          contained++;
+        }
       }
     }
-    return contained/(double)tests;
+    mpz_clear(value);
+    mpz_clear(block_i);
+    mpz_clear(pattern_i);
+    return (double)contained/(double)tests;
   }
 }
 
-vector<boost::dynamic_bitset<>*> SerialFramework::convert_patterns(vector<vector<bool>> patterns){
+vector<boost::dynamic_bitset<>*> SerialFramework::convert_patterns(vector<vector<bool>> patterns) {
   vector<boost::dynamic_bitset<>*> dyn_patterns(patterns.size());
   for(size_t i = 0; i < patterns.size(); i++) {
     boost::dynamic_bitset<>* bits = new boost::dynamic_bitset<>(patterns[0].size());
@@ -55,4 +86,3 @@ vector<boost::dynamic_bitset<>*> SerialFramework::convert_patterns(vector<vector
   }
   return dyn_patterns;
   }
-
--- code-framework/core/serial_framework.h
+++ code-framework/core/serial_framework.h
@@ -5,12 +5,17 @@
 #include <vector>
 #include "patternbloom.h"
 #include <future>
+#include <string>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/uniform_int_distribution.hpp>
 
 using namespace std;
 
 class SerialFramework{
   private:
-    vector<int> source;
+    vector<string> source;
+    vector<string> stored; // To check actual FPR and not true positives.
+    boost::mt19937 random_source;
     vector<boost::dynamic_bitset<>*> convert_patterns(vector<vector<bool>> patterns);
   public:
     SerialFramework();
