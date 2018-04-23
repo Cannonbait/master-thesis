44dfe9e724e975cbf1d0fa792faf56829d57d5f5
Björn Hedström <bjorn@hfinfo.se>
2018-04-23 16:22:57 +0200

    Added input validation in python

--- code-framework/python/analysis.py
+++ code-framework/python/analysis.py
@@ -11,9 +11,11 @@ from pattern_design.che import CHE
 from pattern_design.comp import COMP
 from pattern_design.identity import IDENTITY
 from pattern_design.crs import CRS
+from pattern_design.qary import QARY
+from pattern_design.rcrs import RCRS
 from mpl_toolkits.mplot3d import Axes3D
-sys.argv[1:] = ["-m=512", "-n=1500", "-d=120", "-d_end=160", "-b=1", "-che", "-comp", "-crs", "-step_size=20", "-pattern_trials=1", "-tests=10000"]
-
+#sys.argv[1:] = ["-m=512", "-n=1500", "-d=120", "-d_end=160", "-b=1", "-che", "-comp", "-crs", "-step_size=20", "-pattern_trials=1", "-tests=10000"]
+#sys.argv[1:] = ["-source='../data-preparation/out.prep'", "-m=512", "-n=1500", "-d=6000", "-d_end=20000", "-b=100", "-che", "-crs", "-step_size=1000", "-pattern_trials=1", "-tests=10000"]
 
 ######################## PARSE ARGUMENTS
 def default_arguments():
@@ -23,7 +25,7 @@ def default_arguments():
     arguments["step_size"] = 10
     return arguments
 
-def extract_argument(argv, symbol):
+def _extract_argument(argv, symbol):
     if any([s.startswith("-{0}=".format(symbol)) for s in argv]):
         return int([x for x in sys.argv if x.startswith("-{0}=".format(symbol))][0][len(symbol)+2:])
     elif symbol in default_arguments():
@@ -36,27 +38,26 @@ class Analysis_Settings:
         self.trial_ranges = {}
         range_symbols = ["m", "n", "d", "b"]
         for symbol in range_symbols:
-            self.trial_ranges[symbol] = [extract_argument(argv, symbol)]
-            end = extract_argument(argv, symbol+"_end")
+            self.trial_ranges[symbol] = [_extract_argument(argv, symbol)]
+            end = _extract_argument(argv, symbol+"_end")
             if end != None:
                 self.trial_ranges[symbol].append(end)
 
-        self.tests = extract_argument(argv, "tests")
-        self.pattern_trials = extract_argument(argv, "pattern_trials")
-        self.step_size = extract_argument(argv, "step_size")
+        self.tests = _extract_argument(argv, "tests")
+        self.pattern_trials = _extract_argument(argv, "pattern_trials")
+        self.step_size = _extract_argument(argv, "step_size")
 
+        if any([s.startswith("-compare=") for s in argv]):
+            self.compare = [x for x in sys.argv if x.startswith("-compare=")][0][len("-compare="):]
+        else:
+            self.compare = False
         if any([s.startswith("-source=") for s in argv]):
             self.path = [x for x in sys.argv if x.startswith("-source=")][0][len("-source="):]
             print("Using data from ", self.path)
         else:
             print("Found no \"source\" argument, trials will be run with random input")
+            self.compare = False
             self.path = None
-
-        if any([s.startswith("-compare") for s in argv]):
-            self.compare = True
-        else:
-        	self.compare = False
-
         self.pattern_designs = []
         if any([s.startswith("-che") for s in argv]):
             self.pattern_designs.append(CHE)
@@ -65,18 +66,22 @@ class Analysis_Settings:
         if any([s.startswith("-crs") for s in argv]):
             self.pattern_designs.append(CRS)
         if len(self.pattern_designs) == 0:
-            print("No pattern designs flagged")
+            print("No pattern designs flagged. Exiting...")
             sys.exit(0)
 
+    @staticmethod
+    def create_setting(designs, **arguments):
+        return 0
+
 ######################## GENERATE DATA
-def generate_dimensions(settings):
+def _generate_dimensions(settings):
     dimensions = []
     for key in settings.trial_ranges:
         if len(settings.trial_ranges[key]) == 2:
             dimensions.append((key, np.arange(settings.trial_ranges[key][0], settings.trial_ranges[key][1], settings.step_size)))
     return dimensions
 
-def convert_to_matrix(results, dimensions, settings):
+def _convert_to_matrix(results, dimensions, settings):
     if settings.compare:
         mult = 2
     else:
@@ -93,25 +98,21 @@ def generate_data(settings):
     if not all([issubclass(gen, IPatternGenerator) for gen in settings.pattern_designs]):
         raise ValueError('One or more generators does not implement the IPatternGenerator interface.')
 
-    dimensions = generate_dimensions(settings)
-    parameters = {}
+    dimensions = _generate_dimensions(settings)
+    parameters = _generate_parameters(settings)
     # Set the parameter for all values that are fix (i.e. not a range)
-    for key in settings.trial_ranges:
-        if len(settings.trial_ranges[key]) == 1:
-            parameters[key] = settings.trial_ranges[key][0]
-    # If we have no ranges
+
+
     if len(dimensions) == 0:
-        print("No ranges not implemented")
-        sys.exit(0)
+        raise ValueError('No ranges not supported')
     elif len(dimensions) == 1:
         trials = []
         controller = worker_pool.Controller(settings.path)
         for ix, x in enumerate(dimensions[0][1]):
             parameters[dimensions[0][0]] = x
             trials.append((ix, parameters.copy()))
-
-        return(convert_to_matrix(controller.test(trials, settings), dimensions, settings))
-
+        data = _convert_to_matrix(controller.test(trials, settings), dimensions, settings)
+        return(data)
     else:
         fpr = np.zeros([dimensions[0][1].size, dimensions[1][1].size])
         trial_parameters = parameters.copy()
@@ -123,10 +124,22 @@ def generate_data(settings):
                 fpr[ix][iy] = run_trial(trial_parameters, settings)
         return fpr
 
+def _generate_parameters(settings):
+    parameters = {}
+    for key in settings.trial_ranges:
+        if len(settings.trial_ranges[key]) == 1:
+            parameter = settings.trial_ranges[key][0]
+            if parameter < 1:
+                print("Invalid input. One or more arguments are non-positive.")
+                print("Exiting...")
+                sys.exit(0)
+            parameters[key] = parameter
+    return parameters
+
 ######################## DISPLAY DATA
 
 def display_data(result, deviation, settings):
-    dimensions = generate_dimensions(settings)
+    dimensions = _generate_dimensions(settings)
     if len(dimensions) == 1:
         fig, ax = plt.subplots()
         x = dimensions[0][1]
