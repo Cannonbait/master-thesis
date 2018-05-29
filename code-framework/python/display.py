40c6cbae3c6a72acfe3a9b9a66c103a4209491da
Björn Hedström <bjorn@hfinfo.se>
2018-05-29 17:01:40 +0200

    Fixed things

--- code-framework/python/display.py
+++ code-framework/python/display.py
@@ -6,15 +6,15 @@ import numpy as np
 
 _display_colors = ['tab:orange', 'b', 'g', 'c', 'm', 'r', 'k']
 
-def display_data(result, deviation, settings):
+def display_data(results, settings):
     dimensions = _generate_dimensions(settings)
     if len(dimensions) > 2:
         raise ValueError("Displaying data over three or more ranges is not supported")
 
     if len(dimensions) == 1:
-        _display_one_dimension(result, deviation, settings, dimensions)
+        _display_one_dimension(results, settings, dimensions)
     else:
-        _display_two_dimensions(result, deviation, settings, dimensions)
+        _display_two_dimensions(results, settings, dimensions)
 
 def _generate_dimensions(settings):
     dimensions = []
@@ -23,22 +23,21 @@ def _generate_dimensions(settings):
             dimensions.append((key, np.arange(settings.trial_ranges[key][0], settings.trial_ranges[key][1]+1, settings.trial_ranges[key][2])))
     return dimensions
 
-def _display_one_dimension(result, deviation, settings, dimensions):
+def _display_one_dimension(results, settings, dimensions):
     fig, ax = plt.subplots()
     x = dimensions[0][1]
-    if settings.compare:
-        for i in range(0,(result.shape)[1],2):
-            ax.errorbar(x,result[:,i],deviation[:,i],fmt='-o',c=_display_colors[i//2])
-            ax.errorbar(x,result[:,i+1],deviation[:,i+1],fmt='s--',c=_display_colors[i//2])
-        labels = []
-        for p_design in settings.pattern_designs:
-            labels.append('Empirical '   + p_design.get_name())
-            labels.append('Theoretical ' + p_design.get_name())
-        plt.legend(labels)
-    else:
-        for i in range((result.shape)[1]):
-            ax.errorbar(x,result[:,i],deviation[:,i],fmt='-o')
-        plt.legend([p_design.get_name() for p_design in settings.pattern_designs])
+    for result in results:
+        print(result)
+        for iy, pattern_design in enumerate(settings.pattern_designs):
+            res = [x[iy] for x in result[0]]
+            dev = [x[iy] for x in result[1]]
+            ax.errorbar(x, res, dev,fmt='-o')
+
+    labels = []
+    for source in settings.sources:
+        for pattern_design in settings.pattern_designs:
+            labels.append(source + " - " + pattern_design.get_name())
+    plt.legend(labels)
 
     if(settings.interpret):
         label = _interpret_parameter(dimensions[0][0])
@@ -50,19 +49,26 @@ def _display_one_dimension(result, deviation, settings, dimensions):
     plt.grid(True)
     plt.show()
 
-def _display_two_dimensions(result, deviation, settings, dimensions):
+def _display_two_dimensions(results, settings, dimensions):
     fig = plt.figure()
     ax = fig.gca(projection='3d')
     x, y = np.meshgrid(dimensions[1][1], dimensions[0][1])
-    for index, val in enumerate(result[0][0]):
-        res = [[dim2[index] for dim2 in dim1] for dim1 in result]
-        ax.plot_surface(x,y,np.asarray(res),color=_display_colors[index])
+
+    print("a")
+    for ix, result in enumerate(results):
+        for iy, pattern_design in enumerate(settings.pattern_designs):
+            res = [[dim2[iy] for dim2 in dim1] for dim1 in result[0]]
+            color = iy + ix*len(settings.pattern_designs)
+            ax.plot_surface(x,y,np.asarray(res),color=_display_colors[color])
+    print("b")
+
     # Add legend for planes
     fakeLines = []
     names = []
-    for index, p_design in enumerate(settings.pattern_designs):
-        names.append(p_design.get_name())
-        fakeLines.append(mpl.lines.Line2D([index],[index], linestyle="none", c=_display_colors[index], marker = 'o'))
+    for ix, source in enumerate(settings.sources):
+        for iy, p_design in enumerate(settings.pattern_designs):
+            names.append(source + " - " +p_design.get_name())
+            fakeLines.append(mpl.lines.Line2D([iy],[iy], linestyle="none", c=_display_colors[iy + ix*len(settings.pattern_designs)], marker = 'o'))
     ax.legend(fakeLines, names, numpoints = 1)
 
     if(settings.interpret):
